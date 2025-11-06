547d8e6cff8c4b559c842290c065f392e12c9958
rashpal <rashpalbhagat4@gmail.com>
2025-11-06 23:02:53 +0530

    on the line

---
+++ loops/muliplynumber.py
@@ -0,0 +1,9 @@
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+    return avg
+calc_avg(2, 6, 10)
+
+print("rashpal syal", end= " ")
+print("jalandhar city")
--- variables.py
+++
@@ -1,6 +0,0 @@
-name = "rashpal"
-age = 27
-price = 29.99
-age2 = age
-print("my name is:",name )
-print("my age is",age2)
