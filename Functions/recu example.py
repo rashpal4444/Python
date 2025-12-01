d20214d7c2ddd3ad50f4ffcd8b44beae99815912
Rashpal <rashpalbhagat4@gmail.com>
2025-12-01 23:20:10 +0530

    to be continue

---
+++ Functions/recu example.py
@@ -0,0 +1,7 @@
+# To print natural number.
+def calc_sum(n):
+    if(n == 0):
+        return  0
+    return calc_sum(n-1) + n
+sum = calc_sum(6)
+print(sum)
--- Functions/recursion.py
+++ Functions/recursion.py
@@ -4,4 +4,10 @@ def show(n):
         return
     print(n)
     show(n-1)
-show(5)    
+show(5) 
+
+def fact(l):
+    if(l == 1 or l == 0):
+        return 1
+    return fact(l-1) * l
+print(fact(4))
